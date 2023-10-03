9749474818b62e9fded74875c0999a8f5906a1bd
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-10-03 08:41:11 -0300

    feat: add Interface to models

---
+++ src/models/interface/person_repository_interface.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+
+class PersonRepositoryInterface(ABC):
+
+    @abstractmethod
+    def create_person(self, name, age, neighborhood, profession):
+        pass
+
+    @abstractmethod
+    def find_person_by_name(self, name):
+        pass
+
+    @abstractmethod
+    def delete_person_by_name(self, name):
+        pass
+
+    @abstractmethod
+    def update_person(self, name, age, neighborhood, profession):
+        pass
+
+    @abstractmethod
+    def list_all_people(self):
+        pass
