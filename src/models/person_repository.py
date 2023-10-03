1f80c4587fcfc79141eb59ed2a2532d7095af956
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-10-03 12:34:29 -0300

    fix: refactoring person repository

--- src/models/person_repository.py
+++ src/models/person_repository.py
@@ -1,15 +1,13 @@
 from src.models.interface.person_repository_interface import PersonRepositoryInterface
 from src.models.person_model import PersonModel
+from src.main.server import db
 
 
 class PersonRepository(PersonRepositoryInterface):
-    def __init__(self, db):
-        self.db = db
-
     def create_person(self, name, age, neighborhood, profession):
         person = PersonModel(name=name, age=age, neighborhood=neighborhood, profession=profession)
-        self.db.session.add(person)
-        self.db.session.commit()
+        db.session.add(person)
+        db.session.commit()
 
     def find_person_by_name(self, name):
         person = PersonModel.query.filter_by(name=name).first()
@@ -17,15 +15,15 @@ class PersonRepository(PersonRepositoryInterface):
 
     def delete_person_by_name(self, name):
         person = self.find_person_by_name(name)
-        self.db.session.delete(person)
-        self.db.session.commit()
+        db.session.delete(person)
+        db.session.commit()
 
     def update_person(self, name, age, neighborhood, profession):
         person = self.find_person_by_name(name)
         person.age = age
         person.neighborhood = neighborhood
         person.profession = profession
-        self.db.session.commit()
+        db.session.commit()
 
     def list_all_people(self):
         all_people = PersonModel.query.all()
