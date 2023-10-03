61c7a06630054869b48cc350d34a208de8e18f5f
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-10-03 12:24:59 -0300

    feat: add controllers views

---
+++ src/views/create_person_view.py
@@ -0,0 +1,17 @@
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+from src.views.interface.views_interface import ViewInterface
+from src.controllers.interface.person_controller_interface import PersonInterface
+
+
+class CreatePersonView(ViewInterface):
+    def __init__(self, controller: PersonInterface) -> None:
+        self.__controller = controller
+
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
+        try:
+            person_data = http_request.body
+            response = self.__controller.operate(person_data)
+            return HttpResponse(status_code=200, body=response)
+        except Exception as exception:
+            return HttpResponse(status_code=500, body={"error": str(exception)})
---
+++ src/views/find_person_view.py
@@ -0,0 +1,18 @@
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+from src.views.interface.views_interface import ViewInterface
+from src.controllers.interface.person_controller_interface import PersonInterface
+
+
+class FindPersonView(ViewInterface):
+    def __init__(self, controller: PersonInterface) -> None:
+        self.__controller = controller
+
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
+        try:
+            name = http_request.query_params.get("name")
+            response = self.__controller.operate(name)
+            return HttpResponse(status_code=200, body=response)
+        except Exception as exception:
+            return HttpResponse(status_code=500, body={"error": str(exception)})
+        
