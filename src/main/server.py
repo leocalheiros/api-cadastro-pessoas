481b9490e0bc81579fb46afda8fa929fec43f47c
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-10-04 13:01:01 -0300

    fix: refactoring server.py to works with new config

--- src/main/server.py
+++ src/main/server.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from src.main.config.config import Config
-from .routes import person_blueprint
+from src.main.config.database import db
+
 
 app = Flask(__name__)
 CORS(app)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
+db.init_app(app)
+
+from .routes import person_blueprint
 app.register_blueprint(person_blueprint)
